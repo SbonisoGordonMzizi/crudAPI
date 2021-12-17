b6111971c86d0d507361a7ff12ca6c6b8b64bff5
SbonisoGordonMzizi <gordonmzizi@gmail.com>
2021-12-17 15:07:19 +0200

    creating functions for reading posts. closed #2

--- crudapi.py
+++ crudapi.py
@@ -1,4 +1,5 @@
-from fastapi import FastAPI
+from fastapi import FastAPI,status,Response
+from fastapi.exceptions import HTTPException
 from pydantic import BaseModel
 
 #schema modeling class
@@ -6,6 +7,28 @@ class Post(BaseModel):
     title: str
     content: str
     published_public: bool = True
+
+
+app = FastAPI()
+
+@app.get("/posts")
+def get_post_all(response:Response):
+    #sql
+    data = None
+    if data == None:
+        raise HTTPException(status.HTTP_404_NOT_FOUND, detail="Posts Not Found")
+    else:
+        return data
+
+
+@app.get("/posts/{id}")
+def get_post_id(id:int):
+    #sql
+    data = None
+    if data == None:
+        raise HTTPException(status.HTTP_404_NOT_FOUND, detail="Post Not Found")
+    else:
+        return data
 
 
 
